05fe0560425f84fadbfd95252a131c81b1aaf3fa
Alaukik Tyagi <tyagialaukik@gmail.com>
2026-04-18 12:57:45 +0530

    feat: setup React Router with home and doc routes

---
+++ client/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
+import Home from './pages/Home'
+import DocPage from './pages/DocPage'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doc/:roomId" element={<DocPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
